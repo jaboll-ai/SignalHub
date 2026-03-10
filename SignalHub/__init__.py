8986e95c685877e9880ab519b6c16b5398dad368
jaboll-ai <spexe0011@gmail.com>
2026-03-10 15:50:31 +0100

    added bgr to exposed functions

--- SignalHub/__init__.py
+++ SignalHub/__init__.py
@@ -2,7 +2,7 @@ from .engine import Engine, EngineMode
 from .signals import npTensor, rgbImage, orNone
 from .module import Module
 from .galy import GALY
-from .misc import get_nested_key
+from .misc import get_nested_key, bgr
 from .configparser import ConfigParser
 from .webcam import Webcam
 from .recorder import Recorder, Replay
